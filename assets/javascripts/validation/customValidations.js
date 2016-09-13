821c69588fa8891f73df1136191ba01324dec921
mattpepper <matt.pepper@outlook.com>
2016-09-13 17:18:46 +0100

    JQuery validation for grouped inline field elements
    JQuery validation for grouped inline field elements
    * adds 'formInlineGroup' for handling grouped inline form elements (i.e. dates: dob.day dob.month dob.year):
      - merges generic form validation logic and overrides callbacks where grouped elements present;
      - adds groups setting for validator;
      - display error messages inline with group error;
    * adds 'dateGroupValid' for handling day month year inline fields to check valid date
      - called once individual fields other rules are passed;
      - ensures date is a parse-able calendar date (i.e. 30 Feb 2016 or 31 11 2016 is not accepted)
        + converts 3 character month abbreviations to numeric value
        + converts 2 digit year to 4 digit with logical assumed century

--- assets/javascripts/validation/customValidations.js
+++ assets/javascripts/validation/customValidations.js
@@ -13,10 +13,10 @@ module.exports = function () {
     var validSuggestion = false;
 
     $(suggestions).each(function (index, suggestion) {
-        if (value.toLowerCase() === suggestion.title.toLowerCase() || value === suggestion.value) {
-          validSuggestion = true;
-          return false;
-        }
+      if (value.toLowerCase() === suggestion.title.toLowerCase() || value === suggestion.value) {
+        validSuggestion = true;
+        return false;
+      }
     });
 
     return validSuggestion;
@@ -30,5 +30,61 @@ module.exports = function () {
 
     return regex.test(value);
   });
-  
+
+  // Validate separate day - month - year fields as valid date
+  jQuery.validator.addMethod('dateGroupValid', function(value, element, options) {
+    var isValid = false,
+        opts = options.split(','),
+        params = { group: opts[0], minDate: opts[1], maxDate: opts[2] },
+        idPrefix = '#' + params.group + '-',
+        $fields = { year: $(idPrefix + 'year'), month: $(idPrefix + 'month'), day: $(idPrefix + 'day') },
+        values = { year: parseInt($fields.year.val()), month: parseInt($fields.month.val()) - 1, day: parseInt($fields.day.val()) },
+        monthStrings = $fields.month.data('month-strings'),
+        monthMapping = $.inArray($fields.month.val().toUpperCase(), monthStrings ? monthStrings.split(',') : []),
+        thisMillennium = Math.floor(new Date().getFullYear() / 100) * 100,
+        date, maxDate, minDate;
+
+    // All other rules are valid on this element, so empty the error messages
+    $('#' + element.id + '-error-message').empty();
+
+    values.month = (isNaN(values.month) && monthMapping > -1) ? monthMapping : values.month;
+
+    // Check date is parse-able, i.e. a real calendar date (e.g. not 30 Feb 2016 or 31 11 2016)
+    if (!(isNaN(values.year) || isNaN(values.month) || isNaN(values.day)) && !!(params.maxDate.length && params.minDate.length)) {
+      maxDate = new Date(params.maxDate);
+      minDate = new Date(params.minDate);
+
+      // If two-digit year entered create a four digit year by assuming century intended
+      if (values.year < 100) {
+        // A 2 digit year will be interpreted as 19th century by JavaScript date
+        if (thisMillennium + values.year <= maxDate.getFullYear()) {
+          // ...assume this millennium if year in this millennium lte the maxDateYear
+          values.year = thisMillennium + values.year;
+        }
+        else if (thisMillennium - 100 + values.year >= minDate.getFullYear()) {
+          // ...assume last millennium if year in last millennium gte the minDateYear
+          values.year = thisMillennium - 100 + values.year;
+        }
+      }
+
+      // Parse the fields as a Date
+      date = new Date(values.year, values.month, values.day);
+
+      // Compare the date fields against the input fields
+      isValid = date.getFullYear() === values.year &&
+                date.getMonth() === values.month &&
+                date.getDate() === values.day &&
+                date < maxDate && date > minDate;
+
+      // Show/Hide parse error message
+      if (!isValid) {
+        $(idPrefix + 'parse-error-message').removeClass('hidden');
+      } else {
+        $(idPrefix + 'parse-error-message').addClass('hidden');
+      }
+    }
+
+    return isValid;
+  });
 };
+
